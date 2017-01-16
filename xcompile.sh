26565ff4babd83f86228487bb2c6826ba02dc730
MM <mm@c-base.org>
2017-01-16 17:54:32 +0100

    adapt xcompile.sh to Makefile cahnges

--- xcompile.sh
+++ xcompile.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 make clean
-
-if [ ! -d iniparser ] ; then
-	git clone http://github.com/ndevilla/iniparser
-	cd iniparser 
-	make CC=arm-linux-gnueabihf-gcc AR=arm-linux-gnueabihf-ar RANLIB=arm-linux-gnueabihf-ranlib
-	cd ..
-fi
-make CC=arm-linux-gnueabihf-gcc CFLAGS="-I. -I iniparser/src" LIBS="-L iniparser -liniparser"
+make CC=arm-linux-gnueabihf-gcc AR=arm-linux-gnueabihf-ar
